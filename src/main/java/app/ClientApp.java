2ba444977fbdd585f07cd2661d03c96414bbaa3c
karine.mellata@mail.mcgill.ca <karine.mellata@mail.mcgill.ca>
2019-04-30 12:11:31 +0800

    first commit

---
+++ src/main/java/app/ClientApp.java
@@ -0,0 +1,44 @@
+package app;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.simpledb.AmazonSimpleDBClientBuilder;
+
+public class ClientApp {
+	
+	static AmazonEC2      ec2;
+    static AmazonS3       s3;
+    
+    // Taken from AwsConsoleApp.java sample
+    private static void init() throws Exception {
+
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (~/.aws/credentials).
+         */
+        ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        ec2 = AmazonEC2ClientBuilder.standard()
+            .withCredentials(credentialsProvider)
+            .withRegion("us-west-2")
+            .build();
+        s3  = AmazonS3ClientBuilder.standard()
+            .withCredentials(credentialsProvider)
+            .withRegion("us-west-2")
+            .build();
+    }
+
+}
